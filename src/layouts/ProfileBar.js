76f4d46533c2488a2fdf711500f4f3b0896b194f
Louis Heal <lh420@ic.ac.uk>
2023-08-22 23:27:24 +0100

    edit alt text

--- src/layouts/ProfileBar.js
+++ src/layouts/ProfileBar.js
@@ -6,7 +6,7 @@ const ProfileBar = ({ displayName, avatarUrl, onLogout }) => {
     <div className='flex flex-row justify-between items-center pb-5'>
       <h1 className='text-white text-5xl font-bold'>Welcome, {displayName}!</h1>
       <div className='flex flex-row justify-between gap-4'>
-        <img src={avatarUrl} className='w-14 h-14 rounded-lg' alt="twitch profile picture"/>
+        <img src={avatarUrl} className='w-14 h-14 rounded-lg' alt="twitch avatar"/>
         <LogoutButton onClick={onLogout} />
       </div>
     </div>
