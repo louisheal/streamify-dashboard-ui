5b09131543dedd27091f8d5834f27531e66ef7d1
Louis Heal <lh420@ic.ac.uk>
2023-08-25 14:04:34 +0100

    use min content for wrapped welcome text

--- src/layouts/ProfileBar.js
+++ src/layouts/ProfileBar.js
@@ -4,7 +4,7 @@ import LogoutButton from 'components/LogoutButton';
 const ProfileBar = ({ displayName, avatarUrl, onLogout }) => {
   return (
     <div className='flex flex-row items-center pb-5'>
-      <h1 className='text-white text-3xl lg:text-5xl font-bold'>Welcome, {displayName}!</h1>
+      <h1 className='text-white text-3xl lg:text-5xl font-bold w-min-content'>Welcome, {displayName}!</h1>
       <div className='flex flex-row ml-auto gap-4'>
         <img src={avatarUrl} className='w-14 h-14 rounded-lg' alt="twitch avatar"/>
         <LogoutButton onClick={onLogout} />
