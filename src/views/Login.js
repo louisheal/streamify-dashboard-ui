4668442458080ab56f2160c668beaf82552cf3df
Louis Heal <lh420@ic.ac.uk>
2023-09-02 16:55:01 +0100

    use new data structures

--- src/views/Login.js
+++ src/views/Login.js
@@ -6,7 +6,7 @@ import TwitchLoginButton from 'components/TwitchLoginButton';
 
 const ERROR_MSG = "You are not a Streamify affiliate";
 
-const Login = ({ onLogin, toast }) => {    
+const Login = ({ onLogin, toast }) => {
   return (
     <div className="bg-streamify-gradient flex flex-col items-center gap-8 py-12 overflow-auto w-full h-full">
       <img src={streamifyLogo} alt="Streamify Logo" className="max-w-xl w-1/3 w-min-320 mb-5" />
